adbc77c163073e3cc6089aada362b142192e808b
1iuhuikang <118802030+1iuhuikang@users.noreply.github.com>
2024-12-23 16:01:14 +0800

    Add files via upload
    1-6

---
+++ ex3.py
@@ -0,0 +1,37 @@
+print(" I will count my chickens:")
+#output
+
+print("Hens", 25 + 30 / 6.0)
+#30
+print("Rooster", 100.0 -25 * 3 % 4)
+#97
+
+print("Now I will count the eggs:")
+#output
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6.0)
+#6.75
+
+print("Is it true that 3 + 2 < 5 - 7?")
+#output
+
+print(3 + 2 < 5 - 7)
+#5<-2 false
+
+print("What is 3 + 2?", 3 + 2.0)
+#5
+print("What is 5 - 7?", 5 - 7.0)
+#-2
+
+print("Oh, that's why it's False.")
+#output
+
+print("How about some more.")
+#output
+
+print("Is it greater?", 5 > -2)
+#true
+print("Is it greater or equal?", 5 >= -2)
+#true
+print("Is it less or equal?", 5 <= -2)
+#false
