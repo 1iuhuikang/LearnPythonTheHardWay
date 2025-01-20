d2f4ad8b1fb7b802343383797e0e04c58e9cc326
1iuhuikang <118802030+1iuhuikang@users.noreply.github.com>
2025-01-20 10:48:35 +0800

    update

---
+++ The Basics of Programming/ex34.py
@@ -0,0 +1,7 @@
+animals = ['bear'         #at 0 & 1st
+		   'python3'      #at 1 & 2nd
+		   'peacock'      #at 2 & 3rd
+		   'kangaroo'     #at 3 & 4th
+		   'whale'        #at 4 & 5th
+		   'platypus'     #at 5 & 6th
+]
